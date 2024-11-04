07c6b2d0142cf18e3904a6453f96bd6f60ed23ce
professor nazil <nazilsheikh.emerson@gmail.com>
2024-11-04 20:00:10 +0530

    Update views.py

--- MY_ML/pythonProject/views.py
+++ MY_ML/pythonProject/views.py
@@ -1,4 +1,4 @@
- from django.shortcuts import render
+from django.shortcuts import render
 from django.http import JsonResponse
 import joblib   
 from sklearn.ensemble import RandomForestClassifier   
